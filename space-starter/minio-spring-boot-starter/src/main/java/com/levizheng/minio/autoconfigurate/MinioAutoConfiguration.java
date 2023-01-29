09ed26fdf02bb090baeb67c07628f381736d72c3
zhengyi <levi.zheng@outlook.com>
2023-01-29 16:20:09 +0800

    feat(minio starter): add application runner
    auto create bucket

--- space-starter/minio-spring-boot-starter/src/main/java/com/levizheng/minio/autoconfigurate/MinioAutoConfiguration.java
+++ space-starter/minio-spring-boot-starter/src/main/java/com/levizheng/minio/autoconfigurate/MinioAutoConfiguration.java
@@ -2,11 +2,11 @@ package com.levizheng.minio.autoconfigurate;
 
 import io.minio.MinioClient;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 /**
  * @author levi
@@ -15,16 +15,12 @@ import org.springframework.context.annotation.Configuration;
 @AllArgsConstructor
 @Configuration
 @EnableConfigurationProperties(MinioProperties.class)
-public class MinioAutoConfiguration implements InitializingBean {
+public class MinioAutoConfiguration {
 
     private final MinioProperties properties;
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-
-    }
-
     @Bean
+    @Order(1)
     @ConditionalOnMissingBean
     public MinioClient minioClient() {
         return MinioClient.builder()
